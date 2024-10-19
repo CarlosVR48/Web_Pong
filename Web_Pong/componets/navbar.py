0fb9806a9131fd1e9713a1209121c97ec4d4e597
Carlos Vidal <cvrodriguez999@hotmail.com>
2024-10-19 23:26:24 +0200

    Modifico navbar

---
+++ Web_Pong/componets/navbar.py
@@ -0,0 +1,27 @@
+import reflex as rx
+import datetime
+
+def navbar() -> rx.Component:
+    #anno=datetime.date.today().year
+    #mes = datetime.date.today().month
+    #dia = datetime.date.today().day
+    
+    return rx.vstack(
+        rx.hstack(
+            #rx.color_mode.button(position="top-right"),
+            rx.image("navbar.png",alt="tenis",width="32px",heigt="32px"),
+            rx.text("PING-PONG"),
+            rx.spacer(),
+            rx.text(f"{datetime.date.today().day}/{datetime.date.today().month}/{datetime.date.today().year}"),
+            color="black",
+            width="100%"
+        ),
+        bg="#FFF3BC",
+        position="sticky",
+        border_bottom="2px solid #000000",
+        padding_x="25px",
+        padding_y="15px",
+        top="0",
+        z_index="999",
+        width="100%",
+    )
